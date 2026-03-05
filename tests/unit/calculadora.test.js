d93b58801703e8e1cecbb7f629ae1ca5e4dbfc48
Gustavo Henrique <gustavo.henrique@e-gestao.com.br>
2026-03-05 17:34:16 -0300

    data base

--- tests/unit/calculadora.test.js
+++
@@ -1,7 +0,0 @@
-const calculadora = require("../../models/calculadora.js");
-
-test("Somar 2 + 2 deve retornar 4", () => {
-  const result = calculadora.somar(2, 2);
-  console.log(result);
-  expect(result).toBe(4);
-});
