1c640e4d475ea98306ecf7b5beaca72421ce5295
Gustavo Henrique <gustavo.henrique@e-gestao.com.br>
2026-03-11 20:43:44 -0300

    finaliza end point

--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test("Get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.version).toBeDefined();
+  expect(responseBody.dependencies.max_connections).toBe(100);
+  expect(responseBody.dependencies.opened_connections).toBe(1);
 });
